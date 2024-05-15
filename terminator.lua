521fd4750bda01eb24eb403dffd22d9e491864f8
mr. jingles <165362601+Gonistical@users.noreply.github.com>
2024-05-15 21:08:31 +0300

    Create terminator.lua

---
+++ terminator.lua
@@ -0,0 +1,36 @@
+local function BypassTrolling(text: string) 
+	repeat task.wait() until game:IsLoaded();
+
+	local Players = game:GetService("Players");
+	local Player = Players.LocalPlayer;
+
+	local ScreenGui = Instance.new("ScreenGui");
+	local TextLabel = Instance.new("TextLabel");
+	local AspectRatio = Instance.new("UIAspectRatioConstraint");
+
+	ScreenGui.Name = 'DestroyerV2'
+	TextLabel.Name = text
+	AspectRatio.Name = text
+
+	ScreenGui.Parent = Player.PlayerGui
+	ScreenGui.ResetOnSpawn = false
+	ScreenGui.IgnoreGuiInset = true
+
+	AspectRatio.Parent = TextLabel
+	AspectRatio.AspectRatio = 4.471
+	AspectRatio.AspectType = Enum.AspectType.FitWithinMaxSize
+	AspectRatio.DominantAxis = Enum.DominantAxis.Width
+
+	TextLabel.Parent = ScreenGui
+	TextLabel.Text = text
+	TextLabel.Size = UDim2.new(0, 769,0, 172);
+	TextLabel.TextScaled = true
+	TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextLabel.TextStrokeTransparency = 0
+	TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+	TextLabel.Position = UDim2.new(0.5, 0, 0.5, 0)
+	TextLabel.Font = Enum.Font.SourceSansSemibold
+
+	TextLabel.BackgroundTransparency = 1
+	TextLabel.BorderSizePixel = 0
+end
